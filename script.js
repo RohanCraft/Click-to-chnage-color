b00c84c8b72602a165770ff9e564039039aad522
Rohan kumar sahoo <rohankumar9969@gmail.com>
2025-01-05 15:04:59 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+const container = document.querySelector('.container');
+const red = document.querySelector('.red');
+const green = document.querySelector('.green');
+const blue = document.querySelector('.blue');
+const black = document.querySelector('.black');
+const orange = document.querySelector('.orange');
+const yellow = document.querySelector('.yellow');
+const defaultButton  = document.querySelector('.default');
+
+let newHeading = document.createElement('h1');
+
+function remove(){
+    if(container.contains(newHeading)){
+        container.style.backgroundColor="white";
+        container.removeChild(newHeading);
+        console.log("removed the text and color");
+        
+    }
+}
+function ChangeColor(color,content){
+    container.style.backgroundColor = color;
+    newHeading.innerText =content;
+    container.appendChild(newHeading);
+    console.log("you clicked a button");
+    
+}
+
+
+red.addEventListener('click',()=>{
+    remove();
+    ChangeColor('red',`You clicked the "red" button`);
+    
+});
+green.addEventListener('click',()=>{
+    remove();
+    ChangeColor('green',`You clicked the "green" button`);
+});
+blue.addEventListener('click',()=>{
+    remove();
+    ChangeColor('blue',`You clicked the "blue" button`);
+});
+black.addEventListener('click',()=>{
+    remove();
+    ChangeColor('black',`You clicked the "black" button`);
+});
+orange.addEventListener('click',()=>{
+    remove();
+    ChangeColor('orange',`You clicked the "orange" button`);
+});
+yellow.addEventListener('click',()=>{
+    remove();
+    ChangeColor('yellow',`You clicked the "yellow" button`);
+});
+defaultButton .addEventListener('click',()=>{
+    remove();
+    ChangeColor('white',`You clicked the "default" button`);
+});
